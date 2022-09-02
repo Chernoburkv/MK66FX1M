85eafd556d4b683c60221ba99671257c14f08dfb
Klaus <85108209+Chernoburkv@users.noreply.github.com>
2022-09-02 13:28:04 +0300

    Update processing.h

--- processing.h
+++ processing.h
@@ -1,69 +1,9 @@
-#ifndef Wiring_h
-#define Wiring_h
+#ifndef Processing_h
+#define Processing_h
 
 #include <stdbool.h>
 #include "binary.h"
 
-
-// type_traits interferes with min() and other defines
-// include it early, so we can define these later
-// for Arduino compatibility
-#ifdef __cplusplus
-#include <type_traits>
-// when the input number is an integer type, do all math as 32 bit signed long
-template <class T, class A, class B, class C, class D>
-long map(T _x, A _in_min, B _in_max, C _out_min, D _out_max, typename std::enable_if<std::is_integral<T>::value >::type* = 0)
-{
-	long x = _x, in_min = _in_min, in_max = _in_max, out_min = _out_min, out_max = _out_max;
-	// Arduino's traditional algorithm
-#if 0
-	return (x - in_min) * (out_max - out_min) / (in_max - in_min) + out_min;
-#endif
-#if 0
-	// st42's suggestion: https://github.com/arduino/Arduino/issues/2466#issuecomment-69873889
-	if ((in_max - in_min) > (out_max - out_min)) {
-		return (x - in_min) * (out_max - out_min+1) / (in_max - in_min+1) + out_min;
-	} else {
-		return (x - in_min) * (out_max - out_min) / (in_max - in_min) + out_min;
-	}
-#endif
-	long in_range = in_max - in_min;
-	long out_range = out_max - out_min;
-	if (in_range == 0) return out_min + out_range / 2;
-	long num = (x - in_min) * out_range;
-	if (out_range >= 0) {
-		num += in_range / 2;
-	} else {
-		num -= in_range / 2;
-	}
-	long result = num / in_range + out_min;
-	if (out_range >= 0) {
-		if (in_range * num < 0) return result - 1;
-	} else {
-		if (in_range * num >= 0) return result + 1;
-	}
-	return result;
-	// more conversation:
-	// https://forum.pjrc.com/threads/44503-map()-function-improvements
-}
-// when the input is a float or double, do all math using the input's type
-template <class T, class A, class B, class C, class D>
-T map(T x, A in_min, B in_max, C out_min, D out_max, typename std::enable_if<std::is_floating_point<T>::value >::type* = 0)
-{
-	return (x - (T)in_min) * ((T)out_max - (T)out_min) / ((T)in_max - (T)in_min) + (T)out_min;
-}
-//#include <algorithm> // this isn't really needed, is it?  (slows down compiling)
-#include <utility>
-// https://forum.pjrc.com/threads/44596-Teensyduino-1-37-Beta-2-(Arduino-1-8-3-support)?p=145150&viewfull=1#post145150
-template<class A, class B>
-constexpr auto min(A&& a, B&& b) -> decltype(a < b ? std::forward<A>(a) : std::forward<B>(b)) {
-  return a < b ? std::forward<A>(a) : std::forward<B>(b);
-}
-template<class A, class B>
-constexpr auto max(A&& a, B&& b) -> decltype(a < b ? std::forward<A>(a) : std::forward<B>(b)) {
-  return a >= b ? std::forward<A>(a) : std::forward<B>(b);
-}
-#else // not C++
 #define min(a, b) ({ \
   typeof(a) _a = (a); \
   typeof(b) _b = (b); \
@@ -74,7 +14,6 @@ constexpr auto max(A&& a, B&& b) -> decltype(a < b ? std::forward<A>(a) : std::f
   typeof(b) _b = (b); \
   (_a > _b) ? _a : _b; \
 })
-#endif
 
 
 #ifdef PI
@@ -86,13 +25,10 @@ constexpr auto max(A&& a, B&& b) -> decltype(a < b ? std::forward<A>(a) : std::f
 #define DEG_TO_RAD 0.017453292519943295769236907684886
 #define RAD_TO_DEG 57.295779513082320876798154814105
 
-#ifndef M_PI
-#define M_PI 3.1415926535897932384626433832795
-#endif
-#ifndef M_SQRT2
-#define M_SQRT2 1.4142135623730950488016887
-#endif
 
+
+#define SQRT2 1.4142135623730950488016887
+#define SQRT3 1.7320508075688772935274463
 #define SERIAL  0
 #define DISPLAY 1
 
@@ -126,27 +62,8 @@ constexpr auto max(A&& a, B&& b) -> decltype(a < b ? std::forward<A>(a) : std::f
   _x * _x; \
 })
 
-#ifdef __cplusplus
-extern "C"{
-#endif
-
-extern double exp10(double x);
-extern float exp10f(float x);
-extern long double exp10l(long double x);
-extern double pow10(double x);
-extern float pow10f(float x);
-extern long double pow10l(long double x);
 
 #define stricmp(a, b) strcasecmp(a, b)
-
-#define sei() __enable_irq()
-#define cli() __disable_irq()
-#define interrupts() __enable_irq()
-#define noInterrupts() __disable_irq()
-
-#define clockCyclesPerMicrosecond() ( F_CPU / 1000000L )
-#define clockCyclesToMicroseconds(a) ( (a) / clockCyclesPerMicrosecond() )
-#define microsecondsToClockCycles(a) ( (a) * clockCyclesPerMicrosecond() )
 
 #define lowByte(w) ((uint8_t)((w) & 0xFF))
 #define highByte(w) ((uint8_t)((w) >> 8))
@@ -156,28 +73,8 @@ extern long double pow10l(long double x);
 #define bitClear(value, bit) ((value) &= ~(1UL << (bit)))
 #define bitWrite(value, bit, bitvalue) ((bitvalue) ? bitSet((value), (bit)) : bitClear((value), (bit)))
 
-typedef unsigned int word;
-
 #define bit(b) (1UL << (b))
 
-typedef uint8_t byte;
+typedef bool uint8_t ;
 
-uint32_t pulseIn(uint8_t pin, uint8_t state, uint32_t timeout);
-void shiftOut(uint8_t dataPin, uint8_t clockPin, uint8_t bitOrder, byte val);
 
-void setup(void);
-void loop(void);
-
-#ifdef __cplusplus
-} // extern "C"
-#endif
-
-// fix C++ boolean issue
-// https://github.com/arduino/Arduino/pull/2151
-#ifdef __cplusplus
-typedef bool boolean;
-#else
-typedef uint8_t boolean;
-#endif
-
-#endif
